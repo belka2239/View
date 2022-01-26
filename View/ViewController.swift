a9bb2800da97fd5d98fb965d7a78429f556d1171
Gurina Elena <belka2239@gmail.com>
2022-01-26 18:24:22 +0300

    Initial Commit

---
+++ View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  View
+//
+//  Created by VG on 26.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
